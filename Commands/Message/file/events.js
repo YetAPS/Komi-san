d1f502209c49bd7cf687e3e29936465c0c59e932
ferbxyz <181633215+ferbxyz@users.noreply.github.com>
2026-03-26 20:02:51 +0530

    XYZ Beta 2

--- Commands/Message/file/events.js
+++ Commands/Message/file/events.js
@@ -3,20 +3,22 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 let msgCOUNT = 0;
-const targetCOUNT = 50; 
+const targetCOUNT = 46; 
 client.on("messageCreate", async (message) => {
+  const phrases = ["The next person to message is the true knighthood leader of the server, komi really likes them! SHHHHHHHH! Don't tell them.", "The server is active, Komi loves this <3.", "Do you really enjoy these messages?", "Komi needs friends. Anybody? ", "Does anybody use discord?"];
+const getPhrase = phrases[Math.floor(Math.random() * phrases.length)];
   if (message.author.bot) return;                 
   if (!message.guild) return;
   const content = message.content || "";
   if (!content) return;
 
   msgCOUNT++;
-
   if (msgCOUNT >= targetCOUNT) {
-     message.channel.send("The server is active, Komi loves this <3.")
-  }
+    message.channel.send(getPhrase);
+    msgCOUNT = 0;
+}
 
-  msgCOUNT = 0;
+  
 });
 
 client.login(process.env.TOKEN);
